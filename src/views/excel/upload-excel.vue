fbb66c121909c6137e24634bc15f28297249bce8
wuminmin <buckwmm@qq.com>
2020-04-01 17:42:03 +0800

    上传清单按钮

--- src/views/excel/upload-excel.vue
+++ src/views/excel/upload-excel.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="app-container">
+    <el-button>上传人员清单</el-button>
+    <el-button>上传充值清单</el-button>
+
     <upload-excel-component :on-success="handleSuccess" :before-upload="beforeUpload" />
     <el-table :data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;">
       <el-table-column v-for="item of tableHeader" :key="item" :prop="item" :label="item" />
@@ -36,6 +39,12 @@ export default {
     handleSuccess({ results, header }) {
       this.tableData = results
       this.tableHeader = header
+
+      console.log('tableData-------------',results)
+
+      console.log('tableHeader-------------',header)
+
+
     }
   }
 }
